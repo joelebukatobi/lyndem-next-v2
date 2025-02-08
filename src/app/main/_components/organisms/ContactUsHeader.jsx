67f6819479c731e25b257c84c143a033b62d1d62
joelebukatobi <joelebuka@gmail.com>
2025-02-08 13:34:08 -0500

    Fixed Vercel Deploy Error

--- src/app/main/_components/organisms/ContactUsHeader.jsx
+++ src/app/main/_components/organisms/ContactUsHeader.jsx
@@ -17,11 +17,11 @@ export const ContactUsHeader = () => {
               <p>Contact Us</p>
               <hr />
             </div>
-            <h3>We'd love to hear from you</h3>
+            <h3>{`We'd love to hear from you`}</h3>
             <p>
-              Have questions, feedback, or ideas to share? Or perhaps you're interested in purchasing one of our
+              {`Have questions, feedback, or ideas to share? Or perhaps you're interested in purchasing one of our
               educational games? Reach out to us! Whether you're curious about our services, looking to partner with us,
-              or simply want to learn more, we're here to connect and collaborate. Let’s make learning fun, together!
+              or simply want to learn more, we're here to connect and collaborate. Let’s make learning fun, together!`}
             </p>
           </div>
         </div>
